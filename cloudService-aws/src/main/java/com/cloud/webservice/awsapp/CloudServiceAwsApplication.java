f1121940dcbdccfd2c92344fa5a28015c03c589c
InnoTree <InnoTree@DESKTOP-AKV2RS7>
2021-07-01 10:41:35 +0900

    z

---
+++ cloudService-aws/src/main/java/com/cloud/webservice/awsapp/CloudServiceAwsApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.webservice.awsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudServiceAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudServiceAwsApplication.class, args);
+	}
+
+}
---
+++ cloudService-aws/src/test/java/com/cloud/webservice/awsapp/CloudServiceAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.webservice.awsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudServiceAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
